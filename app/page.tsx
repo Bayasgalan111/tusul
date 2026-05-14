64549e7aa554372b6e22d0f760123b363be9a729
Bayasgalan111 <baysgalanv@gmail.com>
2026-05-15 02:44:13 +0800

    Initial commit: Litch cinematic landing page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="fixed inset-0 w-full h-full">
+      <iframe
+        src="/litch-cinematic.html"
+        className="w-full h-full border-0"
+        title="LITCH - Learn by Teaching"
+      />
+    </main>
+  )
+}
